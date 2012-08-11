596d6b2ed100628e394e12e162372c83509862dc
cpf <cpf@cpfx.ca>
2012-08-11 15:10:06 -0600

    *whistle*

---
+++ insight/system.php
@@ -0,0 +1,14 @@
+<h1>About PowerVCWSF</h1>
+<h2>Disclaimer!</h2>
+This site is not affiliated with Youth Science Canada nor the Canada-Wide Science Fair. 
+While all the results and calculations shown here are derived from <a href="http://apps.ysf-fsj.ca/virtualcwsf/" target="_blank">the official CWSF results system</a>, they are generated automatically so there may be inaccuracies. I do not guarantee or warrant the accuracy of any of the data presented on this site.
+The data backing this site is not updated live. All primary project and participant details shown here are the same as what is publicly available through YSC's CWSF results system. 
+
+<h2>No seriously, here's the 'About' section</h2>
+No offence to James &amp; co. at <a href="http://www.lightbox.ca/">Lightbox Technologies</a>, but the official CWSF results system is a bit underpowered. Combine that with a bit of procrastination over diplomas, and this is what you get.
+
+<h2>Contact</h2>
+Collin @ <a href="mailto:cpf@cpfx.ca">cpf@cpfx.ca</a><br/>
+<a href="https://github.com/cpfair/powervcwsf" target="_blank">github &raquo;</a>
+
+
