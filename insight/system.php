5c54a30488464ae4b0ed1c790415de855e50f177
cpf <cpf@cpfx.ca>
2013-03-06 16:52:52 -0500

    Changed remaining ysf-fsj.ca links to youthscience.ca

--- insight/system.php
+++ insight/system.php
@@ -1,7 +1,7 @@
 <h1>About PowerVCWSF</h1>
 <h2>Disclaimer!</h2>
 This site is not affiliated with Youth Science Canada nor the Canada-Wide Science Fair. 
-While all the results and calculations shown here are derived from <a href="http://apps.ysf-fsj.ca/virtualcwsf/" target="_blank">the official CWSF results system</a>, they are generated automatically so there may be inaccuracies. I do not guarantee or warrant the accuracy of any of the data presented on this site.
+While all the results and calculations shown here are derived from <a href="https://secure.youthscience.ca/virtualcwsf/" target="_blank">the official CWSF results system</a>, they are generated automatically so there may be inaccuracies. I do not guarantee or warrant the accuracy of any of the data presented on this site.
 The data backing this site is not updated live. All primary project and participant details shown here are the same as what is publicly available through YSC's CWSF results system. 
 
 <h2>No seriously, here's the 'About' section</h2>
