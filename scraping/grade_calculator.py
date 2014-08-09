c810021bc28b4c080be5e879f12eadb6623884e0
Collin Fair <cpf@cpfx.ca>
2014-08-09 18:21:26 -0400

    Grade calculator

---
+++ scraping/grade_calculator.py
@@ -0,0 +1,19 @@
+
+class GradeCalculator:
+	def CalculateForParticipants(participants):
+		for participant in participants:
+			lastAgeCat = None
+			lastAgeCatYear = 0
+			hasCheckpoint = False
+			for finalist in participant.Participations:
+				if lastAgeCat != finalist.Project.AgeCat and lastAgeCat:
+					hasCheckpoint = True
+					break
+				lastAgeCat = finalist.Project.AgeCat
+				lastAgeCatYear = finalist.Project.Year
+
+			if not hasCheckpoint:
+				continue # We tried
+
+			for finalist in participant.Participations:
+				finalist.Grade = lastAgeCat.GradeEnd + (finalist.Year - lastAgeCatYear)
--- scraping/process_project_pages.py
+++ scraping/process_project_pages.py
@@ -1,6 +1,10 @@
 from parser import Parser
 from sql_export import SQLExporter
+from grade_calculator import GradeCalculator
 
 parser = Parser()
 parser.Parse(["proj_pages"])
+
+GradeCalculator.CalculateForParticipants(parser.Participants)
+
 SQLExporter("localhost", "root", None, "powervcwsf").Export(parser)
